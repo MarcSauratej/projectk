26154492fd0764ea87140a55fb965b76b4b6bf9b
Marc Saura <m.saura1dawnuria2020@gmail.com>
2022-05-26 12:52:28 +0200

    home

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -10,12 +10,45 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
 
-    /**
-     * @Route("/", name="home")
-     * @return Response
-     */
+    #[Route('/', name: 'app_home')]
     public function index(Request $request): Response {
         
         return $this->render('home/home.html.twig');
+    }
+
+    #[Route('/manga', name: 'app_manga')]
+    public function manga(Request $request): Response {
+
+        return $this->render('home/manga.html.twig');
+    }
+
+    #[Route('/anime', name: 'app_anime')]
+    public function anime(Request $request): Response {
+
+        return $this->render('home/anime.html.twig');
+    }
+
+    #[Route('/movies', name: 'app_movies')]
+    public function movies(Request $request): Response {
+
+        return $this->render('home/movies.html.twig');
+    }
+
+    #[Route('/specials', name: 'app_specials')]
+    public function specials(Request $request): Response {
+
+        return $this->render('home/specials.html.twig');
+    }
+
+    #[Route('/battles', name: 'app_battles')]
+    public function battles(Request $request): Response {
+
+        return $this->render('home/battles.html.twig');
+    }
+
+    #[Route('/characters', name: 'app_characters')]
+    public function characters(Request $request): Response {
+
+        return $this->render('home/characters.html.twig');
     }
 }
