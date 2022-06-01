62d24d9be4f5ec6ba779350c215c5579f4e4f2e0
Marc Saura <m.saura1dawnuria2020@gmail.com>
2022-06-01 12:56:22 +0200

    view

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,17 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Quiz;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController {
 
+    public function __construct(EntityManagerInterface $entityManager, ManagerRegistry $doctrine)
+    {
+        $this->entityManager = $entityManager;
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(Request $request): Response {
-        
-        return $this->render('home/home.html.twig');
+
+        $repository = $this->doctrine
+        ->getRepository(Quiz::class);
+        $quizzes = $repository->findAll();
+
+        return $this->render('home/home.html.twig', [
+            'quizzes' => $quizzes
+        ]);
+    }
+
+    #[Route('/quiz/{id}', name: 'app_quiz_view')]
+    public function quizView(Quiz $quiz, Request $request, int $id): Response {
+
+        $repository = $this->doctrine->getRepository(Quiz::class);
+        $quiz = $repository->findOneBy([
+            'id' => $id
+        ]);
+
+        return $this->render('home/viewQuiz.html.twig', [
+            'quizzes' => $quiz
+        ]);
     }
 
     #[Route('/manga', name: 'app_manga')]
